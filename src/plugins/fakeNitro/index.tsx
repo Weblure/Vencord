4f864febf32072a555272330a9c2047a4e0c5a6c
Weblure <Weblurejoltik@gmail.com>
2024-09-26 10:58:30 -0700

    Update index.tsx
    Lazy untested fix for static emojis displaying with "GIF" over them for vanilla users

--- src/plugins/fakeNitro/index.tsx
+++ src/plugins/fakeNitro/index.tsx
@@ -946,6 +946,13 @@ export default definePlugin({
                     const emojiString = `<${emoji.animated ? "a" : ""}:${emoji.originalName || emoji.name}:${emoji.id}>`;
 
                     const url = new URL(IconUtils.getEmojiURL({ id: emoji.id, animated: emoji.animated, size: s.emojiSize }));
+                    
+                    // If the emoji is not animated, ensure the file extension is .png
+                    // TODO: Temporary fix for Discord displaying "GIF" over non-animated FakeNitro emoji
+                    if (!emoji.animated) {
+                        url.pathname = url.pathname.replace(/\.\w+$/, '.png');  // Replace any extension with .png
+                    }
+                    
                     url.searchParams.set("size", s.emojiSize.toString());
                     url.searchParams.set("name", emoji.name);
 
